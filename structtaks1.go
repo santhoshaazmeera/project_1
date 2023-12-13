4f9f66bcb9be24a3b37b61ebcf694f68f3780615
santhoshaazmeera <147895291+santhoshaazmeera@users.noreply.github.com>
2023-12-13 21:29:48 +0530

    Add files via upload

--- structtaks1.go
+++ structtaks1.go
@@ -19,15 +19,32 @@ type teacher struct {
 	salary float64
 }
 type student struct {
-	name string
-	age  int
+	name   string
+	age    int
+	salary string
 }
 
 func main() {
 	e := employe{name: "dharani", age: 22, salary: 350000.0}
+	e1 := employe{name: "adhi", age: 23, salary: 350000.0}
+	e2 := employe{name: "sandhya", age: 23, salary: 350000.0}
+	e3 := employe{name: "rakshitha", age: 23, salary: 350000.0}
+	e4 := employe{name: "sharath", age: 25, salary: 350000.0}
+
 	t := teacher{name: "vignan", age: 28, salary: 800000.0}
-	s := student{name: "santhosha", age: 21}
+	t1 := teacher{name: "narasimham", age: 58, salary: 900000.0}
+	t2 := teacher{name: "sudhakar", age: 28, salary: 600000.0}
+	s := student{name: "santhosha", age: 21, salary: "no salary fo students"}
+	s1 := student{name: "venkky", age: 23, salary: "no salary fo students"}
+
 	implementingmethods(e)
+	implementingmethods(e1)
+	implementingmethods(e2)
+	implementingmethods(e3)
+	implementingmethods(e4)
+	implementingmethods(t1)
+	implementingmethods(t2)
+	implementingmethods(s1)
 	implementingmethods(t)
 	implementingmethods(s)
 }
@@ -37,18 +54,24 @@ func implementingmethods(u user) {
 	u.salaryy()
 }
 func (e employe) details() {
-	fmt.Println("printing the eploye details ", e.name, e.age)
+	fmt.Printf("printing the employee details \n\n" )
+	fmt.Printf("name  :%s    age is : %d \n", e.name, e.age)
 }
 func (s student) details() {
-	fmt.Println("student details are ", s.name, s.age)
+	fmt.Printf("student details \n\n ")
+	fmt.Printf("name  :%s and   is %d\n", s.name, s.age)
 }
 func (t teacher) details() {
-	fmt.Println("teacher details are ", t.name, t.age)
+	fmt.Printf("teacher details \n \n ")
+	fmt.Printf("name  :%s   age  :%d \n", t.name, t.age)
 }
 func (e employe) salaryy() {
-	fmt.Println("printing the eploye details ", e.salary)
+	fmt.Printf("employee %s salary details %f \n", e.name, e.salary)
 }
 
 func (t teacher) salaryy() {
-	fmt.Println("teacher details are ", t.salary)
+	fmt.Printf("teacher %s salary details %f\n\n", t.name, t.salary)
+}
+func (s student) salaryy() {
+	fmt.Println(s.salary, "\n")
 }
